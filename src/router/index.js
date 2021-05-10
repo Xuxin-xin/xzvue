30299cc2505bf56a86190eda38d6fb7820b0ba79
xu <xu@163.com>
2021-05-10 09:52:12 +0800

    第1次提交

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Index from "../views/Index.vue";
+import Details from "../views/Details.vue";
+import Products from "../views/Products.vue";
+import NotFound from "../views/NotFound.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [
+        { path: "/", component: Index },
+        { path: "/details", component: Details },
+        { path: "/products", component: Products},
+        { path: "*", component: NotFound},
+    ],
+});
+export default router
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h3>你访问的页面不存在</h3>
+      <h3>Not Found ：400</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
